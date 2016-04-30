4ce72dfff37691f7a1a0bd06b0289e90fd55253d
Nick Embrey <cne.embrey@gmail.com>
2016-04-30 16:56:26 -0700

    Pass first AppTest rootTest.

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.ArrayList;
+
+public class App {
+  public static void main(String[] args) {
+
+    staticFileLocation("/public");
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
+
+  }
+}
