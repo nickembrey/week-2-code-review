16c57d49a5f07e8112f2982830af8a0371e94b08
Nick Embrey <cne.embrey@gmail.com>
2016-04-30 15:59:06 -0700

    Pass first DefinitionTest definition_instantiatesCorrectly_true.

---
+++ src/main/java/Definition.java
@@ -0,0 +1,13 @@
+public class Definition {
+
+  private String mContent;
+
+  public Definition(String content) {
+    mContent = content;
+  }
+
+  public String getContent() {
+    return mContent;
+  }
+
+}
